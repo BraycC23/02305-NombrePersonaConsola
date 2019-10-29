8588ce0483b7424237da1ff54692efde50052949
BRAYAN <BRAYAN@192.168.1.111>
2019-10-29 16:35:58 +0100

    Ejercicio 05 Tema 023

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,44 @@
+/* 
+Copyright 2019  Brayan C. Sanchez Arenas - brayancamilo.sanchez.alum2iescap.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Brayan C. Sanchez Arenas
+ */
+public class Main {
+public static final Scanner SCN = 
+	  new Scanner(System.in, "Windows-1252")
+	  .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    public static void main(String[] args) {
+        // Variables
+        String nombre;
+        //Introducción datos
+        try{
+            System.out.print("Introduzca su nombre...:  ");
+            nombre = SCN.nextLine();
+            
+            //Mensaje
+            System.out.printf("Me llamo .............: %s%n", nombre);
+        } catch (Exception e){
+            System.out.println("¡Error Introducción de datos!");
+        }
+    }
+}
